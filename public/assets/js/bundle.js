91398d6275b01b186024b91a08f6957f09862247
Leogom26 <leogomecommerce@gmail.com>
2025-08-24 14:00:16 -0300

    xhr e formidable

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -16,12 +16,53 @@ class DropBoxController {
   constructor() {
     this.btnSendFile = document.querySelector("#btn-send-file");
     this.inputFilesEl = document.querySelector("#files");
+    this.snackModalEl = document.querySelector("#react-snackbar-root");
+
     this.initEvents();
   }
 
   initEvents() {
-    this.btnSendFile.addEventListener("click", (e) => {});
-    this.inputFilesEl.click();
+    this.btnSendFile.addEventListener("click", (e) => {
+      this.inputFilesEl.click();
+    });
+
+    this.inputFilesEl.addEventListener("change", (e) => {
+      this.uploadTask(e.target.files);
+
+      this.snackModalEl.style.display = "block";
+    });
+  }
+
+  uploadTask(files) {
+    let promises = [];
+    [...files].forEach((file) => {
+      promises.push(
+        new Promise((resolve, reject) => {
+          let xhr = new XMLHttpRequest();
+
+          xhr.open("POST", "/upload");
+
+          xhr.onload = (event) => {
+            try {
+              resolve(JSON.parse(xhr.responseText));
+            } catch (error) {
+              reject(error);
+            }
+          };
+
+          xhr.onerror = (event) => {
+            reject(event);
+          };
+
+          let formData = new FormData();
+          formData.append("input-file", file);
+
+          xhr.send(formData);
+        })
+      );
+    });
+
+    return Promise.all(promises);
   }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var formidable = require("formidable");
+const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/upload", (req, res) => {
+  const form = new formidable.IncomingForm({
+    uploadDir: path.join(__dirname, "..", "uploads"), // garante que os arquivos sejam salvos em /uploads
+    keepExtensions: true, // mantém a extensão do arquivo
+  });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error("Erro ao fazer upload:", err);
+      return res.status(500).json({ error: "Erro no upload" });
+    }
+
+    // Retorna algum feedback útil
+    return res.json({
+      success: true,
+      file: files["input-file"], // o nome do campo usado no formData no front-end
+    });
+  });
 });
 
 module.exports = router;
